6f38c7d4e375a3cb1c5527f5be173145e241fdd6
Neil Kalman <Neilkalman@gmail.com>
2017-11-10 02:36:27 +0200

    new thingy - JS

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-setInterval(function() {
-    $('.zero-animation').toggleClass('turn-to-b');
-    $('.one-animation').toggleClass('turn-to-i');
-    $('#logo').toggleClass('active');
-}, 5000);
+var refreshIntervalId = changeBetween();
+var _changeZerosAndOnes;
 
 $('.toggler#colors').click(function() {
     $('#logo').toggleClass('only-white');
@@ -11,3 +8,33 @@ $('.toggler#colors').click(function() {
 $('.toggler#kt').click(function() {
     $('#logo').toggleClass('always-k-t');
 });
+
+$('.toggler#loader').click(function() {
+    if (refreshIntervalId) {
+        clearInterval(refreshIntervalId);
+        refreshIntervalId = null;
+        $('#logo').addClass('loader');
+    } else {
+        refreshIntervalId = changeBetween();
+        $('#logo').removeClass('loader');
+    }
+    $('#logo').removeClass('shape-active');
+})
+
+function changeBetween() {
+    $('#logo').removeClass('shape-active');
+
+    return setInterval(function() {
+        $('#logo').toggleClass('shape-active');
+    }, 5000);
+}
+
+function changeZerosAndOnes() {
+    return setInterval(function() {
+        var ones = $('.shape-one');
+        var zeros = $('.shape-zero');
+        
+        ones.removeClass('shape-one').addClass('shape-zero');
+        zeros.removeClass('shape-zero').addClass('shape-one');
+    }, 500);
+}
